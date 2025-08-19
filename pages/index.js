ba647aeddb82f1c34398eca30c0c58a5b1faa371
Nikhil Sundriya <nikhilchoudhary2508@gmail.com>
2025-08-19 10:27:30 +0530

    Portfolio

---
+++ pages/index.js
@@ -0,0 +1,22 @@
+import Ubuntu from "../components/ubuntu";
+import ReactGA from 'react-ga4';
+import Meta from "../components/SEO/Meta";
+
+const TRACKING_ID = process.env.NEXT_PUBLIC_TRACKING_ID;
+
+if (TRACKING_ID) {
+  ReactGA.initialize(TRACKING_ID);
+} else {
+  console.warn("Google Analytics Tracking ID not found!");
+}
+
+function App() {
+  return (
+    <>
+      <Meta />
+      <Ubuntu />
+    </>
+  );
+}
+
+export default App;
